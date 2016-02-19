492b60bc1f851ee6d3a6417f77cb1589b1d0296f
Sergio Hernández Reyes <sergioreyes_90290@hotmail.com>
2016-02-19 22:42:54 +0000

    Uso de Request y Response de HttpComponent

--- index.php
+++ index.php
@@ -1,6 +1,20 @@
 <?php
+require_once __DIR__.'/vendor/autoload.php';
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-echo "POO";
+$request = Request::createFromGlobals();
 
+$input = $request->get('name', 'World');
+
+$response = new Response(sprintf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
+/*$response = new Response();
+$response->setContent('Esta es una prueba');
+$response->setStatusCode(200);
+$response->headers->set('Content-Type', 'text/html');
+
+$response->setMaxAge(10);*/
+
+$response->send();
 
 ?>
