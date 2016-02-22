4101f11e9d4ff741603a6d558de8257271bf227d
Sergio Hernández Reyes <sergioreyes_90290@hotmail.com>
2016-02-22 16:56:50 +0000

    First version framework

--- web/front.php
+++ web/front.php
@@ -1,6 +1,5 @@
 <?php
-require_once __DIR__.'/vendor/autoload.php';
-
+require_once __DIR__.'/../vendor/autoload.php';
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -8,14 +7,15 @@ $request = Request::createFromGlobals();
 $response = new Response();
 
 $map = array(
-    '/hello' => __DIR__.'/hello.php',
-    '/bye' => __DIR__.'/bye.php',
+    '/hello' => __DIR__.'/../src/pages/hello.php',
+    '/bye' => __DIR__.'/../src/pages/bye.php',
     );
 
 $path = $request->getPathInfo();
-
 if(isset($map[$path])){
-    require $map[$path];
+    ob_start();
+    include $map[$path];
+    $response->setContent(ob_get_clean());
 }else{
     $response->setStatusCode(404);
     $response->setContent('Not Found');
