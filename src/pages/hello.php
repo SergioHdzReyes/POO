77ba4155d2b74e9174d0c6776bceeb8846fa890f
Sergio Hernandez Reyes <sergioreyes_90290@hotmail.com>
2016-02-26 00:08:56 -0600

    Cambios desde c9, Routing

--- src/pages/hello.php
+++ src/pages/hello.php
@@ -1,13 +1,1 @@
-<?php
-$name = $request->get('name','World');
-//$response->setContent(sprintf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
-?>
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Hello World</title>
-</head>
-<body>
-    <h1>Hello <?= htmlspecialchars($name, ENT_QUOTES,'UTF-8') ?></h1>
-</body>
-</html>
+Hello <?= htmlspecialchars($name, ENT_QUOTES, 'UTF-8') ?>
