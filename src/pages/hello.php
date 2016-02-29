17852160eb0c89fb2905617a3359c72c1a2cade5
Sergio Hernández Reyes <sergioreyes_90290@hotmail.com>
2016-02-29 21:53:06 +0000

    -

--- src/pages/hello.php
+++ src/pages/hello.php
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-Hello <?php echo htmlspecialchars($name, ENT_QUOTES, 'UTF-8') ?>
-=======
 Hello <?= htmlspecialchars($name, ENT_QUOTES, 'UTF-8') ?>
->>>>>>> 77ba4155d2b74e9174d0c6776bceeb8846fa890f
