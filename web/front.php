3629eb03374f11a00767dfb09df2f084209478a5
Sergio Hernández Reyes <sergioreyes_90290@hotmail.com>
2016-02-29 21:20:15 +0000

    Mejor administracion de paso de parametros por URLs

--- web/front.php
+++ web/front.php
@@ -2,24 +2,27 @@
 require_once __DIR__.'/../vendor/autoload.php';
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing;
 
 $request = Request::createFromGlobals();
-$response = new Response();
+$routes = require __DIR__.'/../src/app.php';
 
-$map = array(
-    '/hello' => 'hello',
-    '/bye' => 'bye',
-    );
+$context = new Routing\RequestContext();
+$context->fromRequest($request);
+$matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 
-$path = $request->getPathInfo();
-if(isset($map[$path])){
+try{
+    extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
     ob_start();
-    extract($request->query->all(), EXTR_SKIP);
-    include sprintf(__DIR__.'/../src/pages/%s.php', $map[$path]);
+    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
+    
     $response = new Response(ob_get_clean());
-}else{
+}catch(Routing\Exception\ResourceNotFoundException $e){
     $response = new Response('Not Found', 404);
+}catch(Exception $e){
+    $response = new Response('An error ocurred', 500);
 }
+
 $response->send();
 
 ?>
