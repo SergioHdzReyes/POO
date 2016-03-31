0d59ba80f4433d2233354aed2165102b8771d561
Sergio Hernández Reyes <sergioreyes_90290@hotmail.com>
2016-03-31 23:10:35 +0000

    Templating

--- web/front.php
+++ web/front.php
@@ -4,6 +4,14 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
 
+function render_template($request){
+    extract($request->attributes->all(), EXTR_SKIP);
+    ob_start();
+    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
+    
+    return new Response(ob_get_clean());
+}
+
 $request = Request::createFromGlobals();
 $routes = require __DIR__.'/../src/app.php';
 
@@ -11,13 +19,9 @@ $context = new Routing\RequestContext();
 $context->fromRequest($request);
 $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 
-d($matcher);
 try{
-    extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
-    ob_start();
-    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
-    
-    $response = new Response(ob_get_clean());
+    $request->attributes->add($matcher->match($request->getPathInfo()));
+    $response = call_user_func('render_template', $request);
 }catch(Routing\Exception\ResourceNotFoundException $e){
     $response = new Response('Not Found', 404);
 }catch(Exception $e){
@@ -25,5 +29,3 @@ try{
 }
 
 $response->send();
-
-?>
